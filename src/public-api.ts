92f3300dc25f9b935f2c36f98c2e05c5adc12003
MadiBlh <madiats2@gmail.com>
2022-04-29 09:52:31 +0200

    delete unused files

--- src/public-api.ts
+++ src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of ngx-basic-carousel
  */
 
-export * from './lib/ngx-basic-carousel.service';
 export * from './lib/ngx-basic-carousel.component';
 export * from './lib/ngx-basic-carousel.module';
